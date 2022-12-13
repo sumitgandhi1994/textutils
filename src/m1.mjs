666aa226c0df67e0a36bd5968c19db037e8afa51
SumitGandhiLaptop <sumitgandhi1994@gmail.com>
2022-12-13 14:36:17 +0530

    All latest update

---
+++ src/m1.mjs
@@ -0,0 +1,6 @@
+import ui, { a, b, c } from "./m2.mjs";
+
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ src/m2.mjs
@@ -0,0 +1,9 @@
+const a = "Sumit";
+const b = "Sumit Gandhi";
+const c = "Sumit Nath Gandhi";
+const d = "Sumit Indar Nath Gandhi";
+
+export default d;
+export {a};
+export {b};
+export {c};
